bb77e25c33b5ef5a4d20cec21a512fde35134fd1
Jericho Irvin <aldejerichoirvin@gmail.com>
2020-11-06 17:33:49 +0800

    Reassigned rendering of firebase firestore collections to redux instead of the shop page while implementing redux thunk, and creating containers for collections-overview and collections page

--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,6 +1,35 @@
-import ShopACtionType from "./shop.types";
+import ShopActionType from "./shop.types";
 
-export const updateCollection = (collectionsMap) => ({
-  type: ShopACtionType.UPDATE_COLLECTION,
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.util";
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionType.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionType.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap,
 });
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionType.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
